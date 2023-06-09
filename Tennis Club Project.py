1ea3f03654da5d9b9d7f1432b48eb4bf19f5b7ca
Black Baron <133782030+BlackBaron94@users.noreply.github.com>
2023-06-09 15:54:10 +0300

    Update Tennis Club Project.py
    Changelog:
    9/6/2023
    Κάποια μηνύματα διαμορφώθηκαν για να είναι πιο ευπαρουσίαστα σε μικρό παράθυρο.
    Διορθώθηκε το σφάλμα απάντησης του ναι/όχι με "ι" που οδηγούσε στη διπλή εκχώρηση παίκτη
    Πλέον ελέγχεται από όλες τις λειτουργίες που χρειάζονται εκχωρήσεις αν η κατάταξη είναι κενή πριν κληθεί οποιαδήποτε συνάρτηση.

--- Tennis Club Project.py
+++ Tennis Club Project.py
@@ -26,10 +26,9 @@ class Challenge:
         # η διαφορά μπορεί να είναι μέχρι 3 θέσεις, ενώ για τις θέσεις απο 9 - ...  μέχρι 4 θέσεις.
         k5 = 4 if self.player1 > 9 else 3  # Αντί για   if player1 <= 9 ....
         if self.player1 - self.player2 > k5:
-            return print(f"Ο παίκτης που προκαλείται βρίσκεται {k5} θέσεις πάνω από τον παίκτη που προκαλεί."
-                  f" Η πρόκληση είναι άκυρη.")
+            return print(f"Ο παίκτης που προκαλείται βρίσκεται {k5} θέσεις πάνω από τον παίκτη που προκαλεί.\nΗ πρόκληση είναι άκυρη.")
         elif self.player1 - self.player2 < 0:
-            return print(f"Ο παίκτης που προκαλείται είναι κάτω από τον παίκτη που προκαλεί. Η πρόκληση είναι άκυρη.")
+            return print(f"Ο παίκτης που προκαλείται είναι κάτω από τον παίκτη που προκαλεί. \nΗ πρόκληση είναι άκυρη.")
         elif self.player1 == self.player2:
             print('Λάθος καταχώρηση.')
             return exit
@@ -37,9 +36,8 @@ class Challenge:
         print("Η πρόκληση είναι αποδεκτή.")
         answer = input(f"Νίκησε ο παίκτης στη θέση #{self.player1} που έκανε την πρόκληση;").upper()
         
-        while answer not in ('ΝΑΊΝΑΙΟΧΙΌΧΙ'):
-            answer = input(f"Νίκησε ο παίκτης στη θέση #{self.player1} που έκανε την πρόκληση; "
-                           f"Παρακαλώ απαντήστε με 'Ναι' ή 'Όχι'. ").upper()
+        while answer not in ('ΝΑΊΝΑΙΟΧΙΌΧΙ') or answer == 'Ι':
+            answer = input(f"Νίκησε ο παίκτης στη θέση #{self.player1} που έκανε την πρόκληση;\nΠαρακαλώ απαντήστε με 'Ναι' ή 'Όχι'. ").upper()
         if answer in ('ΝΑΊΝΑΙ'):
             win(self.player1, self.player2)
         if answer in ('ΌΧΙΟΧΙ'):
@@ -87,19 +85,16 @@ def initialization(players,today_string=today_string):
 #Print όλη την κατάταξη
 def print_():
     '''Τυπώνει την κατάταξη με όλα τα στοιχεία σε μορφοποιημένη ευανάγνωστη διάταξη (πλην Control_Date, για αυτή χρησιμοποιήστε print_ranking())'''
-    if empty_check(1):
-        print('Η κατάταξη δεν περιέχει παίκτες!')
-    else:
-        my_conn = dbconnect('tennis_club.db')
-        c = my_conn.cursor()
-        print(("{:<7}  {:<13}   {:<23}  {:<7}  {:<7}".format('Θέση', 'Όνομα', 'Επίθετο', 'Νίκες', 'Ήττες')))
-        print('*' * 64)
-        result = c.execute("SELECT * FROM ranking;")
+    my_conn = dbconnect('tennis_club.db')
+    c = my_conn.cursor()
+    print(("{:<7}  {:<13}   {:<23}  {:<7}  {:<7}".format('Θέση', 'Όνομα', 'Επίθετο', 'Νίκες', 'Ήττες')))
+    print('*' * 64)
+    result = c.execute("SELECT * FROM ranking;")
         
-        for rec in result.fetchall():
-            print("{:<7}  {:<13}   {:<23}  {:<7}  {:<7}".format(*rec))
-        print('*' * 64)
-        my_conn.close()
+    for rec in result.fetchall():
+        print("{:<7}  {:<13}   {:<23}  {:<7}  {:<7}".format(*rec))
+    print('*' * 64)
+    my_conn.close()
 
     
 #Εισαγωγή παίκτη στο τέλος της κατάταξης, προεπιλεγμένες τιμές για Wins & Loses = 0
@@ -116,7 +111,7 @@ def insert_bottom(Name='', Surname='', Wins=0, Loses=0, Control_Date=today_strin
     
     entry = (new_last_place, Name, Surname, Wins, Loses, today_string) #Πλειάδα στοιχείων παίκτη
     c.execute("INSERT INTO ranking VALUES {0};".format(entry)) #Εκχώρηση παίκτη σε αυτή τη θέση
-    print(f'Ο {Name} {Surname} τοποθετήθηκε στη θέση #{new_last_place}')
+    print(f'Ο {Name} {Surname} τοποθετήθηκε επιτυχώς στη θέση #{new_last_place}.')
     
     my_conn.commit()
     my_conn.close()
@@ -146,7 +141,7 @@ def insert_place(Rank, Name='', Surname='', Wins=0, Loses=0, Control_Date=today_
         info = (Rank, Name, Surname, Wins, Loses, today_string)
         #Εισαγωγή στην κατάταξη είτε η λίστα έχει παίκτες, είτε δεν έχει και ο χρήστης διάλεξε θέση 1
         c.execute("INSERT INTO ranking VALUES {0};".format(info)) 
-        print(f'Ο παίκτης {Name} {Surname} τοποθετήθηκε επιτυχώς στη θέση #{Rank} με {Wins} νίκες και {Loses} ήττες.')
+        print(f'Ο παίκτης {Name} {Surname} τοποθετήθηκε επιτυχώς στη θέση #{Rank}.')
 
         my_conn.commit()
         my_conn.close()
@@ -156,9 +151,7 @@ def insert_place(Rank, Name='', Surname='', Wins=0, Loses=0, Control_Date=today_
 #Διαγραφή παίκτη
 def delete_player(index):
     '''Διαγράφει τον παίκτη στη θέση που δίνεται από το index και μετακινεί τους κατώτερους παίκτες μία θέση πάνω, καλύπτοντας το κενό που δημιουργείται'''
-    if empty_check(1):
-        print("Η κατάταξη είναι άδεια!")
-    elif empty_check(index): #Έλεγχος αν η θέση περιέχει άτομο
+    if empty_check(index): #Έλεγχος αν η θέση περιέχει άτομο
         print(f"Δεν υπάρχει παίκτης στη θέση που προσπαθείτε να κάνετε διαγραφή.")
     else:
         my_conn = dbconnect('tennis_club.db')
@@ -191,9 +184,7 @@ def win(winner_index, loser_index,today_string=today_string):
 
         y = x.fetchall() #Επιστρέφει λίστα, με το y[0] να είναι πλειάδα στοιχείων του νικητή
         c.execute("DELETE FROM ranking WHERE Position={0};".format(winner_index)) #Διαγραφή νικητή από προηγούμενη θέση
-        print(y)
         entry = (loser_index, y[0][1], y[0][2], y[0][3], y[0][4], y[0][5]) #Νέα πλειάδα για αλλαγή "Position"
-        print(entry)
         my_conn.commit()
         my_conn.close()
 
@@ -245,7 +236,7 @@ def rank_decay(index,today_string=today_string):
     inactive_player = x.fetchall() #Αποθήκευση στοιχείων του παίκτη που υπόκειται σε rank decay
     
     if last_place == inactive_player[0][0]: #Έλεγχος για την περίπτωση που ο παίκτης είναι τελευταίος
-        print("Ο παικτης {0} {1} είναι ήδη στην τελευταία θέση και δεν πέφτει περαιτέρω.".format(
+        print("Ο παικτης {0} {1} είναι ήδη στην τελευταία θέση \nκαι δεν πέφτει περαιτέρω.".format(
             inactive_player[0][1], inactive_player[0][2]))
         c.execute("UPDATE ranking SET Control_Date='{0}' WHERE Position={1};".format(today_string, index))
         
@@ -325,9 +316,7 @@ while True:
         if choice == 1:
             flag = empty_check(1) 
             if not flag: #Έλεγχος για το αν ο πίνακας περιέχει παίκτες
-                print("Ο πίνακας κατάταξης περιέχει ήδη παίκτες και δεν μπορεί να αρχικοποιηθεί τυχαία. "
-                      "Παρακαλώ, διαγράψτε όλους τους παίκτες ή προσθέστε παίκτες "
-                      "χρησιμοποιώντας κάποια από τις επιλογές.")
+                print("Ο πίνακας κατάταξης περιέχει ήδη παίκτες και δεν μπορεί να αρχικοποιηθεί τυχαία. \n Παρακαλώ, διαγράψτε όλους τους παίκτες ή προσθέστε παίκτες \n χρησιμοποιώντας κάποια από τις επιλογές.")
                 continue;
             players = []
             while True:
@@ -349,7 +338,7 @@ while True:
 
         elif choice == 2:
             answer = input("Θέλετε να εισάγετε τον παίκτη σε συγκεκριμένη θέση; ").upper()
-            while answer not in ('ΝΑΊΝΑΙΟΧΙΌΧΙ'):
+            while answer not in ('ΝΑΊΝΑΙΟΧΙΌΧΙ') or answer == 'Ι':
                 answer = input("Παρακαλώ απαντήστε με Ναι ή Όχι. "
                                "Θέλετε να εισάγετε τον παίκτη σε συγκεκριμένη θέση; ").upper()
                 
@@ -372,25 +361,37 @@ while True:
                 insert_bottom(name,surname)
 
         elif choice == 3:
-            try:
-                index = int(input("Δώστε τη θέση κατάταξης του παίκτη που θέλετε να αφαιρέσετε: "))
-                delete_player(index)
-            except ValueError:
-                print("Παρακαλώ, εισάγετε ακέραιο αριθμό για τη θέση κατάταξης.")
+            if empty_check(1):
+                print("Η κατάταξη δεν περιέχει παίκτες!")
+            else:
+                try:
+                    index = int(input("Δώστε τη θέση κατάταξης του παίκτη που θέλετε να αφαιρέσετε: "))
+                    delete_player(index)
+                except ValueError:
+                    print("Παρακαλώ, εισάγετε ακέραιο αριθμό για τη θέση κατάταξης.")
 
         elif choice == 4:
-            try:
-                p1 = (int(input("Δώστε τη θέση κατάταξης του παίκτη που προκαλεί: ")))
-                p2 = (int(input("Δώστε τη θέση κατάταξης του παίκτη που προκαλείται: ")))
-                p = Challenge(p1,p2)
-            except ValueError:
-                print("Παρακαλώ, εισάγετε ακέραιο αριθμό για τη θέση κατάταξης.")
-        
+            if empty_check(1):
+                print("Η κατάταξη δεν περιέχει παίκτες!")
+            else:
+                try:
+                    p1 = (int(input("Δώστε τη θέση κατάταξης του παίκτη που προκαλεί: ")))
+                    p2 = (int(input("Δώστε τη θέση κατάταξης του παίκτη που προκαλείται: ")))
+                    p = Challenge(p1,p2)
+                except ValueError:
+                    print("Παρακαλώ, εισάγετε ακέραιο αριθμό για τη θέση κατάταξης.")
+            
         elif choice == 5:
-            check_ranking_for_decay()
+            if empty_check(1):
+                print("Η κατάταξη δεν περιέχει παίκτες!")
+            else:
+                check_ranking_for_decay()
 
         elif choice == 6:
-            print_()
+            if empty_check(1):
+                print("Η κατάταξη δεν περιέχει παίκτες!")
+            else:
+                print_()
 
         elif choice == 7:
             break
